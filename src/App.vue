2a5e3c339f26760f879ad116c1c9f2940be75f8a
MatyeusM <33543464+GHMatti@users.noreply.github.com>
2023-11-15 15:28:15 +0100

    Layout changes

--- src/App.vue
+++ src/App.vue
@@ -10,10 +10,10 @@ import SkillTable from './components/SkillTable.vue';
 </script>
 
 <template>
-  <div class="bg-slate-900 text-slate-100 min-w-full min-h-screen">
-    <div class="m-auto w-2/3 p-4">
+  <div class="bg-slate-800 text-slate-100 min-w-full min-h-screen">
+    <div class="m-auto max-w-7xl min-h-screen bg-slate-900 p-4">
       <header>
-        <h1 class="text-2xl mb-2 text-amber-200">SR5e Life Module Calculator</h1>
+        <h1 class="text-2xl mb-2 text-amber-200 border-b border-amber-200">SR5e Life Module Calculator</h1>
       </header>
 
       <main>
