0e5a1575996904bba39ff0dc771a5fdab1a96866
MatyeusM <33543464+GHMatti@users.noreply.github.com>
2023-11-15 15:28:28 +0100

    Remove unneccesary vars

--- src/components/DisplaySkill.vue
+++ src/components/DisplaySkill.vue
@@ -21,7 +21,7 @@
 
   const isDropdownOpen = ref(false);
 
-  function handleWheel(skill, rating, event) {
+  function handleWheel(skill, event) {
     const delta = event.deltaY > 0 ? 1 : -1;
     if (delta === 1)
       skillStore.modifySkillDecrease(skill);
@@ -35,13 +35,13 @@
   <span class="relative">
     <span @click="isDropdownOpen = true" class="hover:text-slate-300 cursor-pointer">{{ name }} {{ rating }}</span>
     <div v-if="isDropdownOpen" class="z-50 left-0 p-2 w-60 origin-top-left border border-white absolute bg-slate-900" style="margin-top: -30px;">
-      <div v-if="!IsGroup" @wheel="handleWheel(name, rating, $event)" class="text-lg text-amber-200">
+      <div v-if="!IsGroup" @wheel="handleWheel(name, $event)" class="text-lg text-amber-200">
         {{ name }} {{ rating }}
       </div>
-      <div v-if="IsGroup || HasGroup" @wheel="handleWheel(Group, GroupRating, $event)" :class="{ 'text-lg text-amber-200 border-b border-amber-200': IsGroup, 'border-b border-slate-100': !IsGroup }">
+      <div v-if="IsGroup || HasGroup" @wheel="handleWheel(Group, $event)" :class="{ 'text-lg text-amber-200 border-b border-amber-200': IsGroup, 'border-b border-slate-100': !IsGroup }">
         {{ Group }} {{ GroupRating }}
       </div>
-      <div v-for="groupSkill in OtherGroupSkills" :key="groupSkill" @wheel="handleWheel(groupSkill.name, groupSkill.rating, $event)" class="pl-8">
+      <div v-for="groupSkill in OtherGroupSkills" :key="groupSkill" @wheel="handleWheel(groupSkill.name, $event)" class="pl-8">
         {{ groupSkill.name }} {{ groupSkill.rating }}
       </div>
     </div>
