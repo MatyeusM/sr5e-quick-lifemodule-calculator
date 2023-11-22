014e8f3e7ce4c15515fa5da5466db75c8204db2a
MatyeusM <150608071+MatyeusM@users.noreply.github.com>
2023-11-22 14:29:53 +0100

    add versioning for display

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@ import TypeSelector from './components/TypeSelector.vue';
 import QualityTable from './components/QualityTable.vue';
 import SkillTable from './components/SkillTable.vue';
 import SettingsDialog from './components/SettingsDialog.vue';
+import version from './version';
 
 </script>
 
@@ -14,7 +15,7 @@ import SettingsDialog from './components/SettingsDialog.vue';
   <div class="bg-slate-800 text-slate-100 min-w-full min-h-screen">
     <div class="m-auto max-w-7xl min-h-screen bg-slate-900 p-4">
       <header class="flex place-content-between border-b border-amber-200 text-amber-200 text-2xl mb-2">
-        <h1>SR5e Life Module Calculator</h1>
+        <h1>SR5e Life Module Calculator <span class="text-sm px-1">{{ version }}</span></h1>
         <settings-dialog class="mr-2" />
       </header>
 
