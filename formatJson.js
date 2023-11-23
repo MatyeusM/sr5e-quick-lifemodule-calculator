eb0ed187b0687c638c48d0a34b105da18d94a81c
MatyeusM <150608071+MatyeusM@users.noreply.github.com>
2023-11-23 14:32:04 +0100

    use neatjson to format

---
+++ formatJson.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { neatJSON } = require('neatjson');
+
+const dataDirectory = 'src/data';
+
+const files = fs.readdirSync(dataDirectory);
+
+const opts = { wrap: 60, padding: 1, afterComma: 1, afterColon: 1 };
+
+files.forEach((file) => {
+  if (path.extname(file) === '.json') {
+    const filePath = path.join(dataDirectory, file);
+    const rawData = fs.readFileSync(filePath, 'utf-8');
+
+    try {
+      const jsonData = JSON.parse(rawData);
+
+      fs.writeFileSync(`${filePath}`, neatJSON(jsonData, opts), 'utf-8');
+
+      console.log(`Writing: ${filePath}`);
+    } catch (error) {
+      console.error(`Error processing ${file}: ${error.message}`);
+    }
+  }
+});
